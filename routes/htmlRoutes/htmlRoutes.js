a37d12653bb7b050331d2d3c21959bba7963a682
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-14 11:51:02 -0500

    set up app.use for data parsing; and tested api get

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -3,6 +3,6 @@
 // GET * should return the index.html file.
 
 // route get path
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// });
--- server.js
+++ server.js
@@ -9,8 +9,20 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
 // set up routes; request, response -- use routes folder
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/apiNotesRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
+
+// set up data parsing
+app.use(express.json()); //for parsing incoming json data
+app.use(express.urlencoded({extened: true})); // for parsing incoming string/array data
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('yo man');
+    console.log(res.send);
+    console.log(req.send);
+});
+
 
 // to make the server run
 app.listen(PORT, () => {
