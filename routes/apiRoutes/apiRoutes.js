e66465ad45ed6f964587a49321515e869d82ce03
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-14 18:15:22 -0500

    added comments to explain code in: server.js and both route.js files to help better and further understanding

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,17 +1,28 @@
+// sets up several API routes using the express router
 // set up api routes
+
+// imports the express router library, the path library, the fs (file system) library, and the uuid library,
+    // which provides utilities for generating unique IDs
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+// imports the db.json file, which stores the notes data
 const note = require('../../db/db.json');
 
 // GET /api/notes read the db.json file and returns all saved notes as json
 
 // show all notes in JSON data
+// defines a route for handling GET requests to the /api/notes URL path
+// when a user navigates to this URL, the server will respond by sending the JSON data stored in the note array using the res.json() method
+    // with that id using the res.json() method
 router.get('/notes', (req, res) => {
     res.json(note);
 });
 
+// defines another route for handling GET requests to the /api/notes:id URL path
+// this route uses a URL parameter :id to match the id of the note the user wants to view
+// when a user navigates to this URL with a specific id, the server will respond by sending the JSON data for the note with that id using the res.json() method
 router.get('/notes', (req, res) => {
     for (let i = 0; i < note.length; i++) {
         if (note[i].id === req.params.id) {
@@ -20,6 +31,11 @@ router.get('/notes', (req, res) => {
     }
 });
 
+// defines a route for handling POST requests to the /api/notes URL path
+// when a user submits a new note via a POST request to this URL, the server will respond by createing a new note object with a unique id   
+    // using the uuidv4() method, adding it to the note array, and then writing the updated note array to the db.json file
+    // using the fs.writeFileSync() method
+// finally, the server will respond by sending the updated note array as JSON using the res.json() method
 // POST /api/notes receives a new note to save on the request body, add it to the db.json file, then returns
     // the new note to the client
 // give each note a unique id when it's saved -- npm i uuid package 
@@ -37,6 +53,11 @@ router.post('/notes', (req, res) => {
     res.json(note);
 });
 
+// defines a route for handling DELETE requests to the /api/notes:id URL path
+// this route urses a URL parameter :id to match the id of the note the user wants to delete
+// when a user submits a DELETE request to this URL with a specific id, the server will respond by finding and removing
+    // the note with that id from the note array, writing the updated note array to the db.json file, and then sending the
+    // update note array as JSON using the res.json()method
 // delete note by user
 router.delete('/notes/:id', (req, res) => {
     for (let i = 0; i < note.length; i++) {
@@ -51,4 +72,12 @@ router.delete('/notes/:id', (req, res) => {
     res.json(note);
 });
 
+// exports the router module for use in the main server file
 module.exports = router;
+
+// summary:
+// sets up several routes for handling API requests in an express server:
+    // one for reading all notes
+    // one for reading a specific note
+    // one for creating a new note
+    // one for deleting a specific note
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
+// basic setup for an express server in node.js
 // create express server
+// 1. import the express library and require it
 const express = require('express');
-//  to set up the actual server
+// 2. set up the actual server -- initializes the app as an express application
 const app = express();
 
+// 3. set the port number to the environmental variable (port) || 3001 by default
 const PORT = process.env.PORT || 3001;
 
 // set up routes; request, response -- use routes folder
+// 4. imports the api routes from the apiRoutes.js file and the HTML routes from the htmlRoutes.js file
 const apiRoutes = require('./routes/apiRoutes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
 
-// set up data parsing
-app.use(express.json()); //for parsing incoming json data
-app.use(express.urlencoded({extended: true})); // for parsing incoming string/array data
-app.use(express.static('public'));
+// 5. sets up middleware to parse incoming requests; set up data parsing
+app.use(express.json()); // middleware for parsing incoming JSON data
+app.use(express.urlencoded({extended: true})); // middleware used for parsing incoming string/array data
+app.use(express.static('public')); // middleware used to serve static files in the public folder
 
 
 // use apiRoutes
+// 6. use the imported API routes by setting up the middleware to use the apiRoutes for any URL starting with /api
 app.use('/api', apiRoutes);
 // use htmlRoutes
+// 7. use the imported HTML routes by setting up the middleware to use the htmlRoutes for any URL starting with /
 app.use('/', htmlRoutes);
 
 
 
 // to make the server run
+// 8. starts the server and tells it to listen on the specified port number
 app.listen(PORT, () => {
     console.log(`Server is now listening on Port ${PORT}. How are you?!`);
 });
